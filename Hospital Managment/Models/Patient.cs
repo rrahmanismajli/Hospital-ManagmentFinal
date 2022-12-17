2ecfbd21d0cabe0233f6a186e7d4d1a1e5e794a0
ernesa-rexhepi <93884609+ernesa-rexhepi@users.noreply.github.com>
2022-12-17 23:10:27 +0100

    Validation For Patient

--- Hospital Managment/Models/Patient.cs
+++ Hospital Managment/Models/Patient.cs
@@ -6,14 +6,25 @@ namespace Hospital_Managment.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is required")]
         public string LastName { get; set; }
+        [Required(ErrorMessage = "Date of birth is required")]
         public DateTime DateOfBirth { get; set; }
+        [Required(ErrorMessage = "Gender is required")]
         public string Gender { get; set; }
+        [Required(ErrorMessage = "Phone Number is required")]
+        [StringLength(13, MinimumLength = 9, ErrorMessage = "Phone Number must be between 9 and 13 numbers")]
         public string PhoneNumber { get; set; }
-        public string Email { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+         public string Email { get; set; }
+        [Required(ErrorMessage = "Adress is required")]
         public string Address { get; set; }
+        [Required(ErrorMessage = "Insurance Provider is required")]
         public string InsuranceProvider { get; set; }
+        [Required(ErrorMessage = "Primary Care Physician  is required")]
         public string PrimaryCarePhysician { get; set; }
 
   
