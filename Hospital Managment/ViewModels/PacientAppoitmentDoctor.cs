36cd8a6f43ae13a77959f80a78da41693584f7f5
HP <HP@DESKTOP-0S4B6VP>
2022-12-18 23:35:19 +0100

    ViewModel

---
+++ Hospital Managment/ViewModels/PacientAppoitmentDoctor.cs
@@ -0,0 +1,13 @@
+﻿using Hospital_Managment.Models;
+
+namespace Hospital_Managment.ViewModels
+{
+    public class PacientAppoitmentDoctor
+    {
+        public List<Patient> Patients { get; set; }
+        public List<Doctor> Doctors { get; set; }
+        public int? PatientId { get; set; }
+        public int? DoctorId { get; set; }
+        public String DepartamentName { get; set; }
+    }
+}
