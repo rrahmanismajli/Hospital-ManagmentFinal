746674159dde72abb5f873cd0e18111d4b5be4aa
rrahmanismajli <99471166+rrahmanismajli@users.noreply.github.com>
2022-12-22 19:38:55 +0100

    Init

---
+++ Hospital Managment/ViewModels/DoctorViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Hospital_Managment.ViewModels
+{
+    public class DoctorViewModel
+    {
+        public int DoctorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Specialty { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
