7954715a431006e724497cea3113bbd83e6a80bf
ernesa-rexhepi <93884609+ernesa-rexhepi@users.noreply.github.com>
2022-12-23 00:44:18 +0100

    Hospitalization ViewModel

--- Hospital Managment/ViewModels/PrescriptionViewModel.cs
+++ Hospital Managment/ViewModels/PrescriptionViewModel.cs
@@ -6,20 +6,7 @@ namespace Hospital_Managment.ViewModels
 {
     public class PrescriptionViewModel
     {
-        public List<Patient> Patient { get; set; }
-        public List<Doctor> Doctor { get; set; }
-        public List<Appointment> Appointment { get; set; } 
-        public string? MedicineName { get; set; }
-        public string? Dosage { get; set; }
-        public string? Frekuency { get; set; }
-         public int? Duration { get; set; }
-
-        public string Summary { get; set; }
-        public int PatientId { get; set; }
-        [ForeignKey("PatientId ")]
-
-        public int? DoctorId { get; set; }
-        public int? AppointmentId { get; set; }
+    
     }
 
 }
